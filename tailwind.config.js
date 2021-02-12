c2bb231d5dea9d0bd9b03d1321ef5a7c97fb3fe9
Jesper Christ <jesperchrist1101@gmail.com>
2021-02-12 12:43:58 +0100

    Da ist der Lachs wieder

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                first: "#EDF7F6",
+                second: "#1C3144",
+                third: "#234D1B",
+                fourth: "#23CE6B",
+                fifth: "#F97B4D"
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
