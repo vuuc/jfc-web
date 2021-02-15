7f3c1fdea95a0776a35bbbd034234f2a6fdf62a1
Jesper Christ <jesperchrist1101@gmail.com>
2021-02-15 18:18:37 +0100

    new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,8 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-            colors: {
-                first: "#EDF7F6",
-                second: "#1C3144",
-                third: "#234D1B",
-                fourth: "#23CE6B",
-                fifth: "#F97B4D"
+            screens: {
+                "xs": "480px",
             }
         },
     },
